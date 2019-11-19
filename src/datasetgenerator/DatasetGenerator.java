2136b94ddebcad2f6b8baf84b0f639b04aa83d52
Liam Wynn <wynnliam@pdx.edu>
2019-11-19 15:54:21 -0800

    I realized theres a better way. So Im committing here, and then doing something else

--- src/datasetgenerator/DatasetGenerator.java
+++ src/datasetgenerator/DatasetGenerator.java
@@ -20,7 +20,7 @@ class DatasetGenerator {
 		FrequencyTable distribution = distribution();
 		// Keeps track of the frequency of values we counted. Use this
 		// to help us choose the next value from our distribution
-		FrequencyTable count = new FrequencyTable();
+		FrequencyTable count = initialCountTable();
 		// The total number of items from our distribution.
 		int sum = sumOfItems(distribution);
 		// The value we will write to our dataset.
@@ -34,13 +34,20 @@ class DatasetGenerator {
 		try {
 			FileWriter writer = new FileWriter("test.output");
 			out = new BufferedWriter(writer);
-			out.write("Hello");
+
+			for(int i = 0; i < sum; i++) {
+				curr = chooseNextItem(distribution, count, prev);
+				count.incrementCount(curr);
+				out.write(curr + "\n");
+				prev = curr;
+			}
+
 		} catch(IOException e) {
 			System.out.println("ERROR: " + e.toString());
 		} finally {
 			if(out != null)
 				out.close();
-		}	
+		}
 	}
 
 	private static FrequencyTable distribution() {
@@ -70,6 +77,33 @@ class DatasetGenerator {
 		return result;
 	}
 
+	private static FrequencyTable initialCountTable() {
+		FrequencyTable result = new FrequencyTable();
+
+		result.insertItem(20, 0);
+		result.insertItem(19, 0);
+		result.insertItem(18, 0);
+		result.insertItem(17, 0);
+		result.insertItem(16, 0);
+		result.insertItem(15, 0);
+		result.insertItem(14, 0);
+		result.insertItem(13, 0);
+		result.insertItem(12, 0);
+		result.insertItem(11, 0);
+		result.insertItem(10, 0);
+		result.insertItem(9, 0);
+		result.insertItem(8, 0);
+		result.insertItem(7, 0);
+		result.insertItem(6, 0);
+		result.insertItem(5, 0);
+		result.insertItem(4, 0);
+		result.insertItem(3, 0);
+		result.insertItem(2, 0);
+		result.insertItem(1, 0);
+
+		return result;
+	}
+
 	private static int sumOfItems(FrequencyTable distribution) {
 		int sum = 0;
 
@@ -78,5 +112,24 @@ class DatasetGenerator {
 		}
 
 		return sum;
+	}
+
+	private static int chooseNextItem(FrequencyTable itemDistribution, FrequencyTable chosenCount, int prev) {
+		int currItem = 0;
+		int currFreq = 0;
+
+		for(Integer item : itemDistribution) {
+			if(currItem == 0) {
+				currItem = item;
+				currFreq = itemDistribution.count(item);
+			}
+
+			if(prev != item && itemDistribution.count(item) > currFreq && chosenCount.count(item) < itemDistribution.count(item)) {
+				currItem = item;
+				currFreq = itemDistribution.count(item);
+			}
+		}
+
+		return currItem;
 	}
 }
