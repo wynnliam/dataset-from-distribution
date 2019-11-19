86d565aa31d4dd976f20d3ce5b5a680603c9b411
Liam Wynn <wynnliam@pdx.edu>
2019-11-19 14:27:56 -0800

    Updated FrequencyTable to use Hashtable and Iterator -- makes main code more readable

--- src/datasetgenerator/DatasetGenerator.java
+++ src/datasetgenerator/DatasetGenerator.java
@@ -2,11 +2,41 @@ package datasetgenerator;
 
 class DatasetGenerator {
 	public static void main(String[] args) {
-		/*FrequencyTable distribution = constructDistrubtion();
+		FrequencyTable distribution = distribution();
+		int sum = 0;
 
-		for(int i = 0; i < distribution.count(); i++) {
-			System.out.println(i + ": " + distribution.getFrequency(i));
-		}*/
+		for(Integer item : distribution) {
+			sum += distribution.count(item);
+			System.out.println(item + "\t: " + distribution.count(item));
+		}
+
+		System.out.println("Total: " + sum);
 	}
 
+	private static FrequencyTable distribution() {
+		FrequencyTable result = new FrequencyTable();
+
+		result.insertItem(20, 5);
+		result.insertItem(19, 10);
+		result.insertItem(18, 25);
+		result.insertItem(17, 50);
+		result.insertItem(16, 100);
+		result.insertItem(15, 250);
+		result.insertItem(14, 500);
+		result.insertItem(13, 1000);
+		result.insertItem(12, 83000);
+		result.insertItem(11, 83000);
+		result.insertItem(10, 83000);
+		result.insertItem(9, 83000);
+		result.insertItem(8, 83000);
+		result.insertItem(7, 83000);
+		result.insertItem(6, 83000);
+		result.insertItem(5, 83000);
+		result.insertItem(4, 83000);
+		result.insertItem(3, 83000);
+		result.insertItem(2, 83000);
+		result.insertItem(1, 83000);
+
+		return result;
+	}
 }
--- src/datasetgenerator/FrequencyTable.java
+++ src/datasetgenerator/FrequencyTable.java
@@ -1,14 +1,31 @@
 package datasetgenerator;
 
-import java.util.Map;
 import java.util.Hashtable;
+import java.util.Set;
 import java.util.Iterator;
 
-class FrequencyTable {
-	Map frequencies;
+class FrequencyTable implements Iterable<Integer> {
+	private Hashtable<Integer, Integer> frequencies;
 
 	public FrequencyTable() {
 		// Keys are the item, the value is its count
 		this.frequencies = new Hashtable<Integer, Integer>();
+	}
+
+	public void insertItem(int item, int count) {
+		this.frequencies.computeIfAbsent(item, k -> count);
+	}
+
+	public int count(int item) {
+		if(this.frequencies.containsKey(item))
+			return this.frequencies.get(item);
+		else
+			return -1;
+	}
+
+	public Iterator<Integer> iterator() {
+		Set<Integer> set = this.frequencies.keySet();
+
+		return set.iterator();
 	}
 }
