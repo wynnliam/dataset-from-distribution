c0b5c336e390e737238afd771be84d8b4d4e71e6
Liam Wynn <wynnliam@pdx.edu>
2019-11-19 14:04:41 -0800

    Added basic getters and setters to frequency table

--- src/datasetgenerator/DatasetGenerator.java
+++ src/datasetgenerator/DatasetGenerator.java
@@ -2,6 +2,37 @@ package datasetgenerator;
 
 class DatasetGenerator {
 	public static void main(String[] args) {
-		System.out.println("Hello!");
+		FrequencyTable distribution = constructDistrubtion();
+
+		for(int i = 0; i < distribution.count(); i++) {
+			System.out.println(i + ": " + distribution.getFrequency(i));
+		}
+	}
+
+	private static FrequencyTable constructDistrubtion() {
+		FrequencyTable result = new FrequencyTable(21);
+
+		result.setFrequency(20, 5);
+		result.setFrequency(19, 10);
+		result.setFrequency(18, 25);
+		result.setFrequency(17, 50);
+		result.setFrequency(16, 100);
+		result.setFrequency(15, 250);
+		result.setFrequency(14, 500);
+		result.setFrequency(13, 1000);
+		result.setFrequency(12, 83000);
+		result.setFrequency(11, 83000);
+		result.setFrequency(10, 83000);
+		result.setFrequency(9, 83000);
+		result.setFrequency(8, 83000);
+		result.setFrequency(7, 83000);
+		result.setFrequency(6, 83000);
+		result.setFrequency(5, 83000);
+		result.setFrequency(4, 83000);
+		result.setFrequency(3, 83000);
+		result.setFrequency(2, 83000);
+		result.setFrequency(1, 83000);
+
+		return result;
 	}
 }
--- src/datasetgenerator/FrequencyTable.java
+++ src/datasetgenerator/FrequencyTable.java
@@ -4,6 +4,25 @@ class FrequencyTable {
 	private int[] frequencies;
 
 	public FrequencyTable(int count) {
+		if(count <= 0)
+			count = 1;
+
 		this.frequencies = new int[count];
+	}
+
+	public int count() { return this.frequencies.length; }
+
+	public int getFrequency(int index) {
+		if(index < 0 || index >= this.frequencies.length)
+			return -1;
+
+		return this.frequencies[index];
+	}
+
+	public void setFrequency(int index, int val) {
+		if(index < 0 || index >= this.frequencies.length)
+			return;
+
+		this.frequencies[index] = val;
 	}
 }
